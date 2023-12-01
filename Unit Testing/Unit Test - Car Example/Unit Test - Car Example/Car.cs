2a4413724ce937261a29e4c873996f5c892ff45b
Mohamed Elenany <mohamedeleanany7@gmail.com>
2023-12-01 20:47:43 +0200

    add new Unit Testing

---
+++ Unit Testing/Unit Test - Car Example/Unit Test - Car Example/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit_Test___Car_Example
+{
+   public class Car
+    {
+       public int speed  { get; set; }  
+       public bool EngineAreWorking {get ; set ;}
+       public String Direction { get; set; }  
+
+       public void DirectionTo()
+       {
+        speed = 29000;
+        EngineAreWorking = true;
+        Direction = "To Cairo";
+       
+       }
+
+
+    }
+}
---
+++ Unit Testing/Unit Test - Car Example/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Unit_Test___Car_Example;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            var car = new Car();
+
+            car.DirectionTo();
+
+            Assert.IsTrue(car.EngineAreWorking);
+            Assert.AreEqual(29000, car.speed);
+            Assert.AreEqual("To Cairo", car.Direction);
+        }
+    }
+}
